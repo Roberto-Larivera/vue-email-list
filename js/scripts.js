d4fc8ceefed21b27e00ea0d441ae3d88d833bcc2
Roberto Larivera <robylariv@gmail.com>
2023-01-31 14:41:41 +0100

    completato esercizio base

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-console.log('int ok');
 const {createApp} = Vue;
 
 createApp ({
     data(){
         return{
-
+            mails: [] 
         }
     },
     methods: {
@@ -13,13 +12,18 @@ createApp ({
     mounted() {
 
 
-
-        axios.get('...')
-        .then(function (response) {
-            // handle success
-            console.log(response);
-        })
-
+        for (let index = 0; index < 10; index++) {
+            //const element = array[index];
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) =>{
+                // handle success
+                // console.log(response);
+                // console.log(response.data);
+                console.log(response.data.response);
+                this.mails.push(response.data.response);
+            })
+        }
+        console.log(this.mails)
 
 
     },
