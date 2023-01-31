d46b4669dc5121d1ef3ffd179e02eb242cdfa103
Roberto Larivera <robylariv@gmail.com>
2023-01-31 16:46:48 +0100

    Varie prove commentate

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,45 @@ const {createApp} = Vue;
 createApp ({
     data(){
         return{
-            mails: [] 
+            mails: [] ,
+            quantity: 10,
+            // mailsThree: [] ,
+
         }
     },
     methods: {
-        
+        // randomMails(){
+        //     for (let index = 0; index < this.quantity; index++) {
+        //         //const element = array[index];
+        //         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        //         .then((response) =>{
+        //             // handle success
+        //             // console.log(response);
+        //             // console.log(response.data);
+        //             console.log(response.data.response);
+        //             this.mailsThree.push(response.data.response);
+        //         })
+        //     }
+        //     console.log(this.mailsThree)
+        //     return this.mailsThree
+        // }
     },
     mounted() {
 
 
-        for (let index = 0; index < 10; index++) {
+        for (let index = 0; index < this.quantity; index++) {
             //const element = array[index];
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) =>{
                 // handle success
-                // console.log(response);
-                // console.log(response.data);
+                console.log(response);
+                console.log(response.data);
                 console.log(response.data.response);
                 this.mails.push(response.data.response);
             })
         }
         console.log(this.mails)
-
+        // this.randomMails()
 
     },
 }).mount('#app')
