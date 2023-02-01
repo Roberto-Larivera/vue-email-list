44b50a339334c86255cedc87efb7fd75c8df4993
Roberto Larivera <robylariv@gmail.com>
2023-02-01 10:16:52 +0100

    Aggiunti esempi

--- js/scripts.js
+++ js/scripts.js
@@ -5,43 +5,52 @@ createApp ({
         return{
             mails: [] ,
             quantity: 10,
-            // mailsThree: [] ,
+            mailsThree: [] ,
+            mailsFour: [] ,
+            mailsFourVif: false ,
 
         }
     },
     methods: {
-        // randomMails(){
-        //     for (let index = 0; index < this.quantity; index++) {
-        //         //const element = array[index];
-        //         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        //         .then((response) =>{
-        //             // handle success
-        //             // console.log(response);
-        //             // console.log(response.data);
-        //             console.log(response.data.response);
-        //             this.mailsThree.push(response.data.response);
-        //         })
-        //     }
-        //     console.log(this.mailsThree)
-        //     return this.mailsThree
-        // }
+        randomMails(mailsInt){
+            for (let index = 0; index < this.quantity; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) =>{
+                    // handle success
+                    // console.log(response);
+                    // console.log(response.data);
+                    // console.log(response.data.response);
+                    mailsInt.push(response.data.response);
+                })
+            }
+            // console.log(this.mailsThree)
+            return mailsInt
+        },
+        randomMailsFour(mailsInt){
+            for (let index = 0; index < this.quantity; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) =>{
+                    // handle success
+                    // console.log(response);
+                    // console.log(response.data);
+                    // console.log(response.data.response);
+                    mailsInt.push(response.data.response);
+                    if(mailsInt.length == this.quantity){
+                        console.log('sono pieno')
+                        this.mailsFourVif = true
+                    }else{
+                        console.log('sono vuoto')
+                    }
+                })
+            }
+            // console.log(this.mailsThree)
+        },
     },
     mounted() {
+        this.randomMails(this.mails);
 
-
-        for (let index = 0; index < this.quantity; index++) {
-            //const element = array[index];
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) =>{
-                // handle success
-                console.log(response);
-                console.log(response.data);
-                console.log(response.data.response);
-                this.mails.push(response.data.response);
-            })
-        }
-        console.log(this.mails)
-        // this.randomMails()
-
+    },
+    created() {
+        this.randomMailsFour(this.mailsFour)
     },
 }).mount('#app')
